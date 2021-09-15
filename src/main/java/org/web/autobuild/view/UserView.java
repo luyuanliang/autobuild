276db1a9e64613d317ff76f194e1d49524282ea0
luyuanliang <luyuanliang_19842163.com>
2021-09-15 17:33:45 +0800

    update project

--- src/main/java/org/web/autobuild/view/UserView.java
+++ src/main/java/org/web/autobuild/view/UserView.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.web.domain.ViewResult;
+import org.web.basemodel.view.ViewResult;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
